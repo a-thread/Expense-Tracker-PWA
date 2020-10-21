d97872362902aa3ddefeacd632799da2df5938c9
a-thread <aiden.threadgoode@gmail.com>
2020-10-21 15:04:11 -0400

    successfully connected to MongoDB

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
+// Dependencies
 const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
 
-const PORT = 3000;
+
+const PORT = process.env.PORT || 3008;
 
 const app = express();
 
@@ -15,8 +17,10 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/budget", {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget-app", {
   useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
   useFindAndModify: false
 });
 
