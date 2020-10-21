6783eb09bbc4d92b894d3a70d7fd866c619d3a16
a-thread <aiden.threadgoode@gmail.com>
2020-10-21 13:43:02 -0400

    updating service-worker with code from activity 13

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,1 +1,60 @@
-console.log("Hi from your service-worker.js file!");
+// Uncomment the lines below
+
+const CACHE_NAME = "static-cache-v1";
+const DATA_CACHE_NAME = "data-cache-v1";
+
+const FILES_TO_CACHE = [
+    '/',
+    '/index.html',
+    '/index.js',
+    '/manifest.webmanifest',
+    '/assets/css/styles.css',
+    '/assets/images/icons/icon-192x192.png',
+    '/assets/images/icons/icon-512x512.png'
+];
+
+// install
+self.addEventListener("install", function (evt) {
+    // pre cache image data
+    evt.waitUntil(
+        caches.open(DATA_CACHE_NAME).then((cache) => cache.add("/api/images"))
+    );
+
+    // pre cache all static assets
+    evt.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => cache.addAll(FILES_TO_CACHE))
+    );
+
+    // tell the browser to activate this service worker immediately once it
+    // has finished installing
+    self.skipWaiting();
+});
+
+self.addEventListener("activate", function (evt) {
+    evt.waitUntil(
+        caches.keys().then(keyList => {
+            return Promise.all(
+                keyList.map(key => {
+                    if (key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
+                        console.log("Removing old cache data", key);
+                        return caches.delete(key);
+                    }
+                })
+            );
+        })
+    );
+
+    self.clients.claim();
+});
+
+self.addEventListener('fetch', function (evt) {
+    // code to handle requests goes here
+});
+
+evt.respondWith(
+    caches.open(CACHE_NAME).then(cache => {
+        return cache.match(evt.request).then(response => {
+            return response || fetch(evt.request);
+        });
+    })
+);
