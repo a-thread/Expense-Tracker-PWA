2eadb66d8caaa0ddb4ba072c7fc6a42c35c72e95
a-thread <aiden.threadgoode@gmail.com>
2020-10-21 16:09:53 -0400

    adding comments and enabling dotenv

--- server.js
+++ server.js
@@ -3,20 +3,22 @@ const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
+require('dotenv').config();
 
-
+// Port
 const PORT = process.env.PORT || 3008;
 
+// Express
 const app = express();
 
 app.use(logger("dev"));
-
 app.use(compression());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use(express.static("public"));
 
+// Connecting to Mongoose
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget-app", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
@@ -27,6 +29,7 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget-app", {
 // routes
 app.use(require("./routes/api.js"));
 
+// starting server
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
 });
